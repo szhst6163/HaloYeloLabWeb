3415f52e7d6aad15cc164a79d1aea61987083dd4
Simon <172683258@qq.com>
2018-12-08 18:17:09 +0800

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="page1">sadasd</button>
+  </div>
+</template>
+<style>
+</style>
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    components: {
+    },
+    mounted(){
+    },
+    methods: {
+      page1(){
+        this.$router.push({name:"page1"})
+      },
+      reset() {
+      },
+      handleSearchComplete(res) {
+      }
+    }
+  }
+</script>
---
+++ src/pages/page1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    ??????
+  </div>
+</template>
+<style>
+</style>
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    components: {
+    },
+    mounted(){
+    },
+    methods: {
+      page1(){
+      },
+      reset() {
+      },
+      handleSearchComplete(res) {
+      }
+    }
+  }
+</script>
